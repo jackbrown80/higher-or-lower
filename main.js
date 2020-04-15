ef3b69f251350379aef350f696ebea37d4c9142c
Jack Brown <brown_jack80@hotmail.com>
2020-04-15 21:08:06 +0100

    change JS and added green background when correct

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
+const buttons = document.querySelectorAll('.button');
+const numberDisplay = document.querySelector('.number');
+const body = document.querySelector('body');
+let message = document.querySelector('.message');
+
+numberDisplay.innerHTML = generateRandomNumber();
+
 let score = 0;
-let message = "Numbers are between 0 and 10"
-document.getElementById("message").innerHTML = message ; 
-document.getElementById("number").innerHTML = generateNumber();
 
-function generateNumber() {
-    return Math.floor(Math.random() * Math.floor(11))
+function generateRandomNumber(){
+    return Math.floor((Math.random() * 10) + 1);
 }
 
-function checkAnswer(choice) {
-    const originalNumber = document.getElementById("number").innerHTML;
-    const newNumber = generateNumber();         
+function handleButtonClick(){
+    const oldNumber = parseInt(numberDisplay.innerHTML);
+    let newNumber = generateRandomNumber();
 
-    if(choice==1 && originalNumber<newNumber){
-        score +=1;
-        message = "Correct!";
-    } else if (choice==2 && originalNumber>newNumber){
-        score +=1;
-        message = "Correct!";
-    } else if (originalNumber == newNumber){
-        message = "Numbers are the same!";     
-    } else {
-        message = "Wrong!";
+    while (oldNumber === newNumber) {
+        newNumber = generateRandomNumber();
     }
+
+    if(this.innerHTML === "Higher" && newNumber > oldNumber){
+        score += 1;
+        body.classList.add('correct');
+        setTimeout(function(){body.classList.remove('correct');}, 250);
+    } if (this.innerHTML === "Lower" && newNumber < oldNumber){
+        score += 1;
+        body.classList.add('correct');
+        setTimeout(function(){body.classList.remove('correct');}, 250);
+    } else {
+
+    }
+
+    message.innerHTML = `Score: ${score}`;
+    numberDisplay.innerHTML = newNumber;
 }
-            
-document.getElementById("message").innerHTML = message ; 
-document.getElementById("number").innerHTML = newNumber;
-document.getElementById("score").innerHTML = "Score: " + score;
+
+buttons.forEach(button => button.addEventListener('click', handleButtonClick))
